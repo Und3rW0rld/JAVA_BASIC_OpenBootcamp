edef738c06d832fe286840ed36c68754f37574dd
Santiago Acevedo Cacua <santiago.acevedo@Quipux.local>
2022-12-23 16:37:44 -0500

    Solution to the first java excercise

---
+++ Proyect1/src/com/company/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Numericos
+        System.out.println("TYPE NUMERIC DATA:");
+        byte bit = 0;
+        short var2 = 10;
+        int Entero = 19990;
+        long Largo = 40L;
+        double Doble = 50.23D;
+        boolean Buleano = false;
+        float flotante = 120.99F;
+        char character = 'c';
+        String str = "This is a String";
+        System.out.println("___Int numbers___");
+        System.out.println("This is a byte: "+bit);
+        System.out.println("This is a short: "+var2);
+        System.out.println("This is an int: "+Entero);
+        System.out.println("This is a long: "+Largo);
+        System.out.println("___Decimal Numbers___");
+        System.out.println("This is a float: "+flotante);
+        System.out.println("This is a double: "+Doble);
+        System.out.println("___Booleans (True or False)___");
+        System.out.println("This is a boolean: "+Buleano);
+        System.out.println("___TEXT TYPE___");
+        System.out.println("This is a char: "+character);
+        System.out.println("And: "+str);
+
+    }
+}
