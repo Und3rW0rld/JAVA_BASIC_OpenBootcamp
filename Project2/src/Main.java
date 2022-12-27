39a26e24e86b32845a18c657d8f3fd924913166d
Santiago Acevedo Cacua <santiago.acevedo@Quipux.local>
2022-12-27 17:29:00 -0500

    Solution excercice 2

---
+++ Project2/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //EL IVA EN COLOMBIA ESTÁ EN EL 19%
+        System.out.println("Ingrese el precio del producto:");
+        Scanner sc = new Scanner(System.in);
+        float price = sc.nextFloat();
+        System.out.print(ivaFunction(price));
+
+    }
+    public static float ivaFunction(float value){
+        return value*1.19f;
+    }
+}
